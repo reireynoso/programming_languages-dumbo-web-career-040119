0600ff8712afcf723640f4b7313662fcdc4e8a2a
Reinald Reynoso <reireynoso@gmail.com>
2019-03-19 03:28:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,7 @@
 def reformat_languages(languages)
   # your code here
   new_language = {}
+<<<<<<< HEAD
   existing_key = ""
   languages.each do |key,value|
     
@@ -15,6 +16,16 @@ def reformat_languages(languages)
     end
   end
   return new_language
+=======
+  languages.each do |key,value|
+    value.each do |key1,value1|
+      new_language[key1] = value1
+      new_language[key1][:style] = key
+      new_language[key1]
+    end
+  end
+  #puts new_language
+>>>>>>> 3d80c1742b5157b079a3fc68b2ae72d7fb8c0182
 end
 
 
